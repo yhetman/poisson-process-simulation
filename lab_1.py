fd1a8ff5e4f1b978ff86b376f249b308c008c678
Yuliia Hetman <juliagetman5@gmail.com>
2020-04-07 23:28:52 +0300

    |added some beauty|

--- lab_1.py
+++ lab_1.py
@@ -2,29 +2,33 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-eps = 21
-t = np.arange(0, eps, 1)
+steps = 21
+t = np.arange(0, steps, 1)
 n = 0
-lambd = 5
+lambd = 2
 
 for i in range (0,100):
     y = []
     x = [0]
 
-    for i in range (0,eps):
+    for i in range (0,steps):
        y.append(random.expovariate(lambd))
 
     for i in range (0,20):
        x.append(x[i]+y[i])
     n = n + x[10]
 
-print("Avarage value of X(10) =", n/100)
+print("Average value of X(10) =", n/100)
 print ()
 
-fig, ax = plt.subplots()
-ax.plot(t, x, linestyle = 'steps-post')
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.set(facecolor = 'pink')
+ax.set(facecolor = 'pink')
+plt.title('Poisson Process',
+        fontdict={'fontname': 'Times New Roman', 'fontsize': 21},
+        y=1.03) 
+ax.plot(t, x, linestyle = 'steps-post',
+        c='magenta')
 plt.show()
-
-
-        
-        
