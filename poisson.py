d11a498aa69c4ff409b6b293cffcf5a0077890a6
Yuliia Hetman <juliagetman5@gmail.com>
2020-04-14 01:53:43 +0300

    |I was wrong: values were not correct BUT now they are ABSOLUTELY right|

--- poisson.py
+++ poisson.py
@@ -12,40 +12,55 @@ def poisson_process(lambd, steps):
     return sums
 
 def draw_graphic(x_ax,y_ax):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    fig, ax = plt.subplots()
     fig.set(facecolor = 'pink')
     ax.set(facecolor = 'pink')
     plt.title('Poisson Process',
         fontdict={ 'fontsize': 21},
         y=1.03, color='navy') 
-    ax.plot(x_ax, y_ax, linestyle = 'steps-post',
-        c='indigo')
+    plt.step(x_ax, y_ax, c='indigo')
     plt.text(15, -2, "Done by YULIIA HETMAN", fontdict={'fontsize': 6})
     plt.show()
 
 
+def mean_counter(sums, t, n):
+    for j, obj in enumerate(sums):
+        if obj > 10:
+            n.append(t[j - 1])
+            break
+    return n
+
+
+def probability_counter(sums, t, lambd):
+    k = 0
+    for j, obj in enumerate(sums):
+        if obj > 3 and  t[j - 1] == (3 * lambd):
+            k = k + 1
+        if obj > 10 and  t[j - 1] == (9 * lambd):
+            k = k + 1
+        if obj > 15 and t[j-1] > (14 * lambd):
+            k = k + 1            
+    if k == 3:
+        return 1
+    return 0
+            
+    
 def main():
+    
     lambd = 2 
-    steps = 40
+    steps = 41
     n = []
-    number_of_tries = 100
+    number_of_tries = 10000
     counter = 0
 
-    t = np.arange(0, steps,1)
+    t = np.arange(0, steps, 1)  
     for i in range (number_of_tries):
         sums = poisson_process(lambd, steps)
-        for j, obj in enumerate(sums):
-            if obj > 10:
-                print("|%2.6f|\t|%d|" % (obj, j))
-                n.append(t[j - 1])
-       # if sums[3] == 3 * lambd and sums[10] == 9 * lambd and sums[15] > 14 * lambd:
-        #    сounter = counter + 1
-
+        counter += probability_counter(sums, t, lambd)
+        n = mean_counter(sums, t, n)
     print("\n\t==> Average value is %.6f" % (np.mean(n)))
     print("\t==> for lambda equals %.2f :" % (lambd))
-    print("\t==> Mean: EX(10) = %.2f" % (10 * pow(lambd, -1)))
-   # print("\t==> P{N(3) = 3 * lambda, N(10) = 9 * lambda, N(15) > 14 * lambda} = %.7f" % (counter / number_of_tries))
+    print("\t==> P{N(3) = 3 * lambda, N(10) = 9 * lambda, N(15) > 14 * lambda} = %.7f" % (counter / number_of_tries))
     draw_graphic(sums, t)
     print("\t|| Done by YULIIA HETMAN :) ||")
 
