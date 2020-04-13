068bf648dde88a7c4bdf9530ee4f52f63a8ee8fd
Yuliia Hetman <juliagetman5@gmail.com>
2020-04-13 23:57:39 +0300

    |DO NOT MAKE MORE THAN 1000000 ITERATIONS|

--- example.py
+++ example.py
@@ -7,23 +7,23 @@ x = np.cumsum(np.random.exponential(0.5,40))
 
 samples = []
 occurences = 0
-for _ in range(1000000):
+iters = 1000000
+for _ in range(iters):
     passed = 0
     x = np.cumsum(np.random.exponential(0.5,40))
     for i, item in enumerate(x):
         if item > 3:
             if y[i-1] == 6:
                 passed += 1
-                break
-            if item > 10 and y[i-1] == 18:
-                passed += 1
-                break
-            if item > 15 and y[i-1] > 28:
-                passed += 1
-                break
+            if item > 10:
+                if y[i-1] == 18:
+                    passed += 1
+            if item > 15:
+                if y[i-1] > 28:
+                    passed += 1
     if passed == 3:
         occurences += 1
-print("The probablity of N(3)=6, N(10) = 18, N(15) > 28 is " + str(occurences/1000000))
+print("The probablity of N(3)=6, N(10) = 18, N(15) > 28 is %.8f" % (occurences/iters))
 
 plt.step(x,y, c='magenta')
 plt.xlabel('time')
