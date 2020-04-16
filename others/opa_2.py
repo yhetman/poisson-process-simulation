446ec3305c095804f16bb9c361618cab955647d1
Yuliia Hetman <juliagetman5@gmail.com>
2020-04-16 21:42:27 +0300

    |fucking bullshit|

--- others/opa_2.py
+++ others/opa_2.py
@@ -14,7 +14,7 @@ print("\nСередне значення : " + str(average/10000))
 for _ in range (10000):
     X = numpy.cumsum(numpy.random.poisson(l/k, 40*k))
     if X[3*k] == 24:
-        if X[10*k] == 7:
+        if X[10*k] == 72:
             if X[15*k] > 112:
                 probability = probability + (1/10000)
 print("\nЙмовірність визначена за допомогою моделювання: " + str(probability))
--- others/opanasenko.py
+++ others/opanasenko.py
@@ -5,11 +5,10 @@ l = 8 #lambda
 time = []
 average = 0;
 cases = 0
-probability = 0.0
-l = pow(l, -1)
-time = numpy.arange(0, 80, 1)
+probability = 0
+time = numpy.arange(0, 160, 1)
 for _ in range (10000):
-    y = numpy.cumsum(numpy.random.exponential(l, 80))
+    y = numpy.cumsum(numpy.random.exponential(1/l, 160))
     for i, one in enumerate(y):
         if one > 10:
             average += time[i - 1]
@@ -17,23 +16,23 @@ for _ in range (10000):
             break
 print("\nСередне значення : " + str(average/cases))
 i = 0
-for _ in range (10000):
+for _ in range (100000):
     cases = 0
-    y = numpy.cumsum(numpy.random.exponential(l, 80))
+    y = numpy.cumsum(numpy.random.exponential(1/l, 160))
     for i, one in enumerate(y):
-        if one > 3:
-            if time[i - 1] == 3 * 8:
-                cases += 1
-        if one > 10:
-            if time[i - 1] == 9 * 8:
-                cases += 1
-        if one > 15:
-            if time[i - 1] > 14 * 8:
-                cases += 1
+        if one > 3 and time[i - 1] == 3 * 8:
+            cases += 1
+        if one > 10 and time[i - 1] == 9 * 8:
+            cases += 1
+        if one > 15 and time[i - 1] > 14 * 8:
+            cases += 1
     if cases == 3:
-        probability += 0.0001
+        probability += 1
+
+probability = probability/100000
 print("\nЙмовірність визначена за допомогою моделювання: " + str(probability))
 print("\nЩо у відсотковому еквіваленті: " + str(probability*100) + "%")
+
 fig, ax = plt.subplots()
 ax.step(y, time, c = 'black')
 plt.show()
