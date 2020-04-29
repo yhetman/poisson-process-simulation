36a7de0ed80bdec0fcfcd6cf2495e2dae6f2068d
Yuliia Hetman <juliagetman5@gmail.com>
2020-04-29 13:56:01 +0300

    |loop_searching|

--- nonhomogeneous_poisson_process/main.py
+++ nonhomogeneous_poisson_process/main.py
@@ -9,15 +9,6 @@ t_periods = np.array([(1, 5),
                     (4, 20),
                     (2, 23)])
 
-#def certain_period(prev_sum, period):
-#    while True:
-#        new  = np.random.exponential(1 / period[0])
-#   if prev_sum[-1] + new <= period[1]:  
-#            prev_sum.append(prev_sum[-1] + new )
-#        else:
-#            prev_sum.append(period[1])
-#            return prev_sum
-
 def process_simulation():
     period_sum = [0]
     for start, end in t_periods:
@@ -28,31 +19,39 @@ def process_simulation():
             else:
                 period_sum.append(end)
                 break
-            #t_sum = simulate_period(t_sum, period)
     return period_sum
 
+
+def loop_searching(X, Y, numb, arr):
+    for index, every in enumerate(X):
+        if every > numb:
+            arr.append(Y[index - 1])
+            return arr
+
+
 def calc_properties():
-    samples = []
-    samples2 = []
+    res_14 = []
+    res_17 = []
     for _ in range(100):
         X = process_simulation()
         Y = range(len(X))
-    
-        for i, item in enumerate(X):
-            if item > 14:
-                samples.append(Y[i-1])
-                break
+        res_14 = loop_searching(X, Y, 14, res_14)
+        res_17 = loop_searching(X, Y, 17, res_17)
+       #for i, item in enumerate(X):
+       #    if item > 14:
+       #        samples.append(Y[i-1])
+       #        break
 
-        for i, item in enumerate(X):
-            if item > 17:
-                samples2.append(Y[i-1])
-                break
-    mean = np.mean(samples)
-    var = np.mean([ pow(X - mean, 2) for X in samples])
-    mean2 = np.mean(samples2)
+       #for i, item in enumerate(X):
+       #    if item > 17:
+       #        samples2.append(Y[i-1])
+       #        break
+    mean = np.mean(res_14)
+    var = np.mean([ pow(X - mean, 2) for X in res_14])
+    mean2 = np.mean(res_17)
     summary = 0
-    for i in range(len(samples)): summary += ((samples[i] - mean) * (samples2[i] - mean2))
-    cov = summary/(len(samples)-1)
+    for i in range(len(res_14)): summary += ((res_14[i] - mean) * (res_17[i] - mean2))
+    cov = summary/(len(res_14) - 1)
     graphic.draw(X, Y)
     return [mean, var, cov]
 
